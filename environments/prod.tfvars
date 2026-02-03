1bec86ac51f8aa2ef29c0b4704adf500b2909fda
Gilbert Young Jr. <gilbertjyoungjr@gmail.com>
2026-02-03 13:35:01 -0600

    add changes to support ec2 deployment

--- environments/prod.tfvars
+++ environments/prod.tfvars
@@ -5,6 +5,12 @@ environment  = "prod"
 aws_region   = "us-east-1"
 project_name = "fivexl-challenge"
 
+# Website hosting: "s3" for S3 static hosting, "ec2" for EC2 with nginx
+hosting_type = "s3"
+
+# EC2 instance type (only used when hosting_type = "ec2")
+# ec2_instance_type = "t3.micro"
+
 tags = {
   CostCenter = "production"
 }
--- variables.tf
+++ variables.tf
@@ -18,6 +18,23 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "hosting_type" {
+  description = "Where to host the website: 's3' for S3 static hosting or 'ec2' for EC2 instance with nginx"
+  type        = string
+  default     = "s3"
+
+  validation {
+    condition     = contains(["s3", "ec2"], var.hosting_type)
+    error_message = "hosting_type must be 's3' or 'ec2'"
+  }
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 instance type for website hosting (only used when hosting_type = 'ec2')"
+  type        = string
+  default     = "t3.micro"
+}
+
 # Tags applied to all resources for organization and cost tracking
 variable "tags" {
   description = "Tags to apply to all resources"
